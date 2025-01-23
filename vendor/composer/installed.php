48d88d925492b97e3b693c78c4653dcb1601db48
JulioCesar <juliolopezvasquez13@gmail.com>
2025-01-23 10:55:27 -0500

    Sincroniza composer.lock con composer.json

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codigoconjuan/app-salon_php_mvc_js_sass',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e0e85d9c9299dfeffe090b6ddd9b88831874bf40',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'codigoconjuan/app-salon_php_mvc_js_sass' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e0e85d9c9299dfeffe090b6ddd9b88831874bf40',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
